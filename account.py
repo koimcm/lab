41750f54d35f07e769a469ccde2e9f6cbd3015fb
koimcm <125946764+koimcm@users.noreply.github.com>
2023-04-18 00:02:02 -0500

    account.py

---
+++ account.py
@@ -0,0 +1,26 @@
+class Account:
+  
+    
+  def __init__(self, name):
+    self.account_name = name
+    self.account_balance = 0
+    
+  def deposit(self, amount):
+    if amount > 0:
+      self.account_balance += self.amount
+      return True
+    else:
+      return False
+      
+  def withdraw(self, amount):
+    if amount <= self.account_balance and amount > 0:
+      self.account_balance -= self.amount
+      return True
+    else:
+      return False
+  
+  def get_balance(self):
+    return self.account_balance
+  
+  def get_name(self):
+    return self.account_name
