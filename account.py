4f91c07eebe3a69ba52d4a8703c8f0665ef84c17
koimcm <125946764+koimcm@users.noreply.github.com>
2023-04-18 00:21:32 -0500

    Update account.py

--- account.py
+++ account.py
@@ -1,26 +1,50 @@
 class Account:
-  
+  '''
+  A class representing a person's bank account.
+  '''
     
   def __init__(self, name):
-    self.account_name = name
-    self.account_balance = 0
+    '''
+    Constructor to make a bank account with 0 dollars to start.
+    :param name: Bank customer's name.
+    '''
+    self.account_name = name  # str
+    self.account_balance = 0.0  # 0.0
     
   def deposit(self, amount):
+    '''
+    Function to add money to the account. 
+    :param amount: Amount of money to be added to account.
+    :return: Boolean based on if deposit was accepted or not.
+    '''
     if amount > 0:
-      self.account_balance += self.amount
+      self.account_balance += self.amount  # account_balance + amount
       return True
     else:
       return False
       
   def withdraw(self, amount):
+    '''
+    Function to withdraw money from account.
+    :param amount: Amount of money to be withdrawn from the account.
+    :return: Boolean based on if the withdrawl was accepted or not.
+    '''
     if amount <= self.account_balance and amount > 0:
-      self.account_balance -= self.amount
+      self.account_balance -= self.amount # account_balance - amount
       return True
     else:
       return False
   
   def get_balance(self):
-    return self.account_balance
+    '''
+    Getter method for account balance.
+    :return: Account's balance.
+    '''
+    return self.account_balance # float
   
   def get_name(self):
-    return self.account_name
+    '''
+    Getter method for account holder's name.
+    :return: Account holder's name.
+    '''
+    return self.account_name # str
